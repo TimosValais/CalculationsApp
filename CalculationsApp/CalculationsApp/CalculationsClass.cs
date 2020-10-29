e5a0514ba36e0a4e13df440031564f056e1d9d2c
Timos Valais <73531450+TimosValais@users.noreply.github.com>
2020-10-29 23:10:32 +0200

    Uploaded the CalculationApp Folder
    Contains two programs
    -The program.cs that uses the class
    -The class that does the calculations called CalculationsClass and prints them out.

---
+++ CalculationsApp/CalculationsApp/CalculationsClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationsApp
+{
+    class CalculationsClass
+    {
+        public int firstNumber;
+        public int secondNumber;
+
+        public CalculationsClass(int firstNumber, int secondNumber)
+        {
+            this.firstNumber = firstNumber;
+            this.secondNumber = secondNumber;
+
+            Console.WriteLine($"The Sum of the two numbers is: {Add(firstNumber,secondNumber)}");
+            Console.WriteLine($"The Difference of the two numbers is: {Subtract(firstNumber,secondNumber)}");
+            Console.WriteLine($"The Product of the two numbers is: {Multiply(firstNumber, secondNumber)}");
+            if(secondNumber != 0)
+                Console.WriteLine($"The Quotient of the two numbers is: {Divide(firstNumber,secondNumber)}");
+            else
+                Console.WriteLine($"One does not simply divide by {secondNumber}");
+        }
+
+        public int Add(int firstNumber, int secondNumber)
+        {
+            int sum;
+            sum = firstNumber + secondNumber;
+            return (sum);
+        }
+        public int Subtract(int firstNumber, int secondNumber)
+        {
+            int diff;
+            diff = firstNumber - secondNumber;
+            return (diff);
+        }
+        public int Divide(int firstNumber, int secondNumber)
+        {
+            int div;
+            if (secondNumber != 0)
+                div = firstNumber / secondNumber;
+            else
+                div = 0;
+            return (div);
+        }
+        public int Multiply(int firstNumber, int secondNumber)
+        {
+            int product;
+            product = firstNumber * secondNumber;
+            return (product);
+        }
+
+    }
+
+
+}
---
+++ CalculationsApp/CalculationsApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please give me the first number : ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Please five me the second Number : ");
+            int secondNumber = int.Parse(Console.ReadLine());
+            CalculationsClass twoNumbers = new CalculationsClass(firstNumber, secondNumber);
+        }
+    }
+}
